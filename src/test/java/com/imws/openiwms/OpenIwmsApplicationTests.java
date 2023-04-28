72ee9a2a07db24ea36209d85b3e2e91a260e54a2
iwmshubfm <132089700+iwmshubfm@users.noreply.github.com>
2023-04-28 22:30:50 +0530

    Framework Setup

---
+++ src/test/java/com/imws/openiwms/OpenIwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imws.openiwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenIwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
