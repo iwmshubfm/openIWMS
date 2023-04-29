37ed02f1e058cacb243ca64fd11f35a99794317b
iwmshubfm <132089700+iwmshubfm@users.noreply.github.com>
2023-04-29 21:21:50 +0530

    Logger added
    Winston logger added

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
-var logger = require('morgan');
+var logger = require('../server/core/logger/Logger');
 /* This code is defining a route for the root URL path "/" using the HTTP GET method. When a user
 navigates to this URL, the server will render the "index" view using the Express template engine and
 pass in an object with the title property set to "Express". */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* This code is defining a route for the URL path "/login" using the HTTP GET method. When a user
 navigates to this URL, the server will render the "login" view using the Express template engine and
 pass in an object with the title property set to "Express". */
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-router.get('/views/:viewName', function(req, res, next) {
-  let viewName = req.params.viewName
-
-  console.error(viewName);
-  res.render('test/test', { title: 'Express' });
-
+router.get('/views/:viewName', function (req, res, next) {
+  const { module, process, task } = req.query;
+  if (module && process && task) {
+    let url = `${module}/${process}/${task}`;
+    logger.info('Accessing view name: ' + url);
+    res.render(url);
+  } else {
+    logger.error('Required parameters not found: ' + JSON.stringify(req.query));
+    res.render('404');
+  }
 });
 module.exports = router;
